470d6354a2622fa1c7d8a5cb015dfdf0fbfc485c
Quentin Guidée <quentin.guidee@gmail.com>
2020-07-03 22:42:51 +0200

    [core] Add Opposite operator

---
+++ Sources/SwiftCAS/Core/Operators/Opposite.swift
@@ -0,0 +1,45 @@
+//
+//  Opposite.swift
+//  
+//
+//  Created by Quentin Guidée on 03/07/2020.
+//
+
+class Opposite: Operator {
+    var argument: Node
+    var sign: Sign {
+        switch argument.sign {
+            case .Positive:
+                return .Negative
+            case .Negative:
+                return .Positive
+            default:
+                return argument.sign
+        }
+    }
+    
+    init(_ argument: Node) {
+        self.argument = argument
+    }
+    
+    func simplify() -> Node {
+        simplify(node: &argument)
+        return Opposite(argument)
+    }
+    
+    func toString() -> String {
+        return "-" + argument.toString()
+    }
+    
+    func toLaTeX() -> String {
+        return "-" + argument.toLaTeX()
+    }
+    
+    func differentiate(of unknown: Unknown) -> Node {
+        return Opposite(argument.differentiate(of: unknown))
+    }
+    
+    func integrate(of unknown: Unknown) -> Node {
+        return Opposite(argument.integrate(of: unknown))
+    }
+}
