b7fef509cff284a09fc02c43f43a7eeaf44f7d9d
Quentin Guidée <quentin.guidee@gmail.com>
2020-07-29 11:56:59 +0200

    [algebra] Matrixes linesCount, columnsCount, det 2x2/3x3

--- Sources/SwiftCAS/Algebra/Matrix.swift
+++ Sources/SwiftCAS/Algebra/Matrix.swift
@@ -6,31 +6,52 @@
 //
 
 public class Matrix: Node {
-    var nodes: [[Node]] = []
-    public var sign: Sign { return .Unknown }
+    public private(set) var lines: [[Node]] = []
     
-    public required init(_ nodes: [[Node]]) {
-        self.nodes = nodes
+    public var linesCount: Int { return lines.count }
+    public var columnsCount: Int { return lines[0].count }
+    
+    public var sign: Sign { return .Unknown }
+    public var determinant: Node {
+        if (linesCount, columnsCount) == (2, 2) {
+            return Addition(
+                Multiplication(lines[0][0], lines[1][1]),
+                Opposite(Multiplication(lines[0][1], lines[1][0]))
+            ).simplify()
+        } else if (linesCount, columnsCount) == (3, 3) {
+            return Addition(
+                Multiplication(lines[0][0], lines[1][1], lines[2][2]),
+                Multiplication(lines[0][1], lines[1][2], lines[2][0]),
+                Multiplication(lines[0][2], lines[1][0], lines[2][1]),
+                Opposite(Multiplication(lines[0][2], lines[1][1], lines[2][0])),
+                Opposite(Multiplication(lines[0][0], lines[1][2], lines[2][1])),
+                Opposite(Multiplication(lines[0][1], lines[1][0], lines[2][2]))
+            ).simplify()
+        }
+        return Double.nan
     }
     
-    public convenience init(_ nodes: [Node]...) {
-        self.init(nodes)
+    public required init(_ lines: [[Node]]) {
+        self.lines = lines
+    }
+    
+    public convenience init(_ lines: [Node]...) {
+        self.init(lines)
     }
     
     public func simplify() -> Node {
-        var simplifiedNodes: [[Node]] = nodes
-        // Transform simpl.count -> lines/columns
-        for i in 0..<simplifiedNodes.count {
-            for j in 0..<simplifiedNodes[0].count {
-                simplifiedNodes[i][j] = simplifiedNodes[i][j].simplify()
+        var simplifiedLines: [[Node]] = lines
+        for i in 0..<linesCount {
+            for j in 0..<columnsCount {
+                simplifiedLines[i][j] = simplifiedLines[i][j].simplify()
             }
         }
-        return Matrix(simplifiedNodes)
+        return Matrix(simplifiedLines)
     }
     
     public func toString() -> String {
         var string: String = "["
-        nodes.forEach { lines in
+        lines.forEach { lines in
             string.append("[")
             lines.forEach { element in
                 string.append("\(element.toString()),")
